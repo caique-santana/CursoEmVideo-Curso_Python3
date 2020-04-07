66c2b9b643648e443ba0b197f20436d78bf48ba1
Caique Santana <caique.si2620@gmail.com>
2020-04-06 21:35:10 -0300

    Aula 013 (repetição usando 'for') e enunciados dos exercícios 46-56

---
+++ PythonExercicios/ex053.py
@@ -0,0 +1,7 @@
+"""Crie um programa que leia uma frase qualquer e diga se ela é um palíndromo, desconsiderando os espaços.
+Ex:
+APOS A SOPA
+A SACADA DA CASA
+A TORRE DA DERROTA
+O LOBO AMA O BOLO
+ANOTARAM A DATA DA MARATONA"""
---
+++ PythonExercicios/ex055.py
@@ -0,0 +1,2 @@
+"""Faça um programa que leia o peso de cinco pessoas.
+No final, mostre qua foi o maior e o menos peso lidos."""
