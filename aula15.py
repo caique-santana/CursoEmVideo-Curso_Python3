f71d3712009eaf8ae4b974deacbdbf5a6f2d0672
Caique Santana <caique.si2620@gmail.com>
2020-04-14 15:15:00 -0300

    Mudando Aula 15 de pasta

--- aula15.py
+++ aula15.py
@@ -1,11 +1,11 @@
-'''n = s = 0
+n = s = 0
 while True:
     n = int(input('Digite um número: '))
     if n == 999:
         break
     s += n
 # print('A soma vale {}'.format(s))
-print(f'A soma vale {s}')'''
+print(f'A soma vale {s}')
 
 nome = 'José'
 idade = 33
