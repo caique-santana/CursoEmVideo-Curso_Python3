e1b8d9ab96807f73b64aaf4fc03cc241238bf128
Caique Santana <caique.si2620@gmail.com>
2020-05-21 17:36:41 -0300

    Exercícios 113-115 feitos e resoluções do Gustavo Guanabara

--- PythonExercicios/ex114 - Site Está Acessível.py
+++ PythonExercicios/ex114 - Site Está Acessível.py
@@ -1,1 +1,11 @@
-#Crie um código em Python que teste se o site Pudim está acessível pelo computador usado.
+# Crie um código em Python que teste se o site Pudim está acessível pelo computador usado.
+import urllib
+import urllib.request
+
+try:
+    site = urllib.request.urlopen('http://www.pudim.com.br')
+except urllib.error.URLError:
+    print('\033[1;31mO site Pudim não está acessível no momento.\033[m')
+else:
+    print('\033[1;32mConsegui acessar o site Pudim com sucesso.\033[m')
+    print(site.read())  # Retorna o conteúdo HTML do site
--- PythonExercicios/ex115 - Criando um Menu em Python.py
+++ PythonExercicios/ex115 - Criando um Menu em Python.py
@@ -1,3 +1,31 @@
 """Crie um pequeno sistema modularizado que permita cadastrar pessoas pelo seu nome e idade
 em um arquivo de texto simples.
 O sistema só vai ter 2 opções: cadastrar uma nova pessoa e listar todas as pessoas cadastradas."""
+from ex115.lib.interface import *
+from ex115.lib.arquivo import *
+from time import sleep
+
+arq = 'cursoemvideo.txt'
+
+if not arquivoExiste(arq):
+    criarArquivo(arq)
+
+while True:
+    resposta = menu(['Ver pessoas cadastradas', 'Cadastrar nova pessoas', 'Sair do Sistema'])
+    if resposta == 1:
+        # Opção de listar o conteúdo de um arquivo!
+        lerArquivo(arq)
+    elif resposta == 2:
+        # Opção de cadasrar uma nova pessoa.
+        cabeçalho('NOVO CADASTRO')
+        nome = str(input('Nome: '))
+        idade = leiaInt('Idade: ')
+        cadastrar(arq, nome, idade)
+    elif resposta == 3:
+        # Opção de sair do sistema.
+        cabeçalho('Saindo do sistema... Até logo!')
+        break
+    else:
+        # Digitou uma opção errada no menu.
+        print('\033[31mERRO! Digite uma opção válida!\033[m')
+    sleep(2)
